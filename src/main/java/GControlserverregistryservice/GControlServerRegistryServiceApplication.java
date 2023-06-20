adf1b72731e38c1d2981e30d30b6bb095aebc3c9
rajanesh.kumar <rajanesh.kumar@qtsolv.com>
2023-06-20 17:02:29 +0530

    initial commits v2

--- src/main/java/GControlserverregistryservice/GControlServerRegistryServiceApplication.java
+++ src/main/java/GControlserverregistryservice/GControlServerRegistryServiceApplication.java
@@ -8,15 +8,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class GControlServerRegistryServiceApplication extends SpringBootServletInitializer {
+public class GControlServerRegistryServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GControlServerRegistryServiceApplication.class, args);
-    }
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(GControlServerRegistryServiceApplication.class);
     }
 
 }
--- src/test/java/GControlserverregistryservice/GControlServerRegistryServiceApplicationTests.java
+++ src/test/java/GControlserverregistryservice/GControlServerRegistryServiceApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class GControlServerRegistryServiceApplicationTests {
 
-	@Test
+	/*@Test
 	void contextLoads() {
-	}
+	}*/
 
 }
